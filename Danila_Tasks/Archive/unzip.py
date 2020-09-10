203d7f25368d5b4f6e06437797a808d2a239b227
VovaRen <67147767+VovaRen@users.noreply.github.com>
2020-09-10 15:11:28 +0500

    Докстринги
    Добавил докстринги

--- Danila_Tasks/Archive/unzip.py
+++ Danila_Tasks/Archive/unzip.py
@@ -1,28 +1,39 @@
 import os.path
 import rarfile
 import shutil
-path_arch = 'C:\\1'
 
 
 def folder():
-    check_file = os.path.exists(path_arch)
+    """Функция проверяет, существует ли каталог
+    на диске С, если да, то удаляет его со всем
+    содержимым.
+    """
+    check_file = os.path.exists('C:\\1')
     if check_file:
-        shutil.rmtree(path_arch, ignore_errors=False, onerror=None)
+        shutil.rmtree('C:\\1', ignore_errors=False, onerror=None)
 
 
 def unzip():
+    """Функция распаковывает архив на диск С.
+    Если каталог не существует, то создаёт его.
+    """
     fantasy_zip = rarfile.RarFile('C:\\Users\\de\\PyCharmProjects\\'
                                   'Exception\\venv\\Archive\\1.rar')
-    fantasy_zip.extractall(path_arch)
+    fantasy_zip.extractall('C:\\1')
 
 
 def files_in_zip():
+    """Функция ищет файлы и папки в каталоге.
+    Выводит список папок и файлов
+    и печатает их кол-во.
+    :return files_in_zip:
+    """
     dirs_list = []
     files_list = []
-    for root, dirs, files in os.walk(path_arch):
+    for root, dirs, files in os.walk('C:\\1'):
         for f in files:
             files_list.append(f.split('.')[0])
-    for r, d, f in os.walk(path_arch):
+    for r, d, f in os.walk('C:\\1'):
         for dirs in d:
             dirs_list.append(dirs)
     print(dirs_list + files_list)
