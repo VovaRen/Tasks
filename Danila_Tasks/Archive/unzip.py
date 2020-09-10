2aa9a2063c486a524e5f4a5b761dd177efc8a2b4
VovaRen <67147767+VovaRen@users.noreply.github.com>
2020-09-10 14:35:59 +0500

    Update unzip.py
    Добавил переменную 'path' и подставил в аргументы функций

--- Danila_Tasks/Archive/unzip.py
+++ Danila_Tasks/Archive/unzip.py
@@ -1,24 +1,25 @@
 import os.path
 import rarfile
 import shutil
+path_arch = 'C:\\1'
 
 
 def folder():
-    check_file = os.path.exists('C:\\1')
+    check_file = os.path.exists(path_arch)
     if check_file:
-        shutil.rmtree('C:\\1', ignore_errors=False, onerror=None)
+        shutil.rmtree(path_arch, ignore_errors=False, onerror=None)
 
 
 def unzip():
     fantasy_zip = rarfile.RarFile('C:\\Users\\de\\PyCharmProjects\\'
                                   'Exception\\venv\\Archive\\1.rar')
-    fantasy_zip.extractall('C:\\1')
+    fantasy_zip.extractall(path_arch)
 
 
 def files_in_zip():
     dirs_list = []
     files_list = []
-    for root, dirs, files in os.walk('C:\\1'):
+    for root, dirs, files in os.walk(path_arch):
         for f in files:
             files_list.append(f.split('.')[0])
     for r, d, f in os.walk('C:\\1'):
